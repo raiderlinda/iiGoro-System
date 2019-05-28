b7e92896290c4e9d3cf6f33af8afe06bf7446df6
raiderlinda <49811957+raiderlinda@users.noreply.github.com>
2019-05-28 22:39:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,6 +38,36 @@ if(!message.channel.guild) return;
     }
 });
 
+client.on("message", message => {
+    var prefix = "-";
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("bc <message>");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                m.send(args);
+            });
+            const AziRo = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)
+            .setTitle(':white_check_mark:| جاري ارسال رسالتك ') 
+            .addBlankField(true)
+            .addField(':hotsprings:| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)
+            .addField(':pencil:| الرسالة ', args)
+            .setColor('RANDOM')
+            message.channel.sendEmbed(AziRo);
+        }
+        } else {
+            return;
+        }
+    });
+
+
 client.on('message', message => {
 var prefix = "-" // البريفكس
 if(message.content.startsWith(prefix +"server")){ // الامر
