2b6aa17eb927253bb6e4f9bbe146cff2f0bd53c2
raiderlinda <49811957+raiderlinda@users.noreply.github.com>
2019-05-28 22:05:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`#,, Paris`,"http://twitch.tv/Death Shop")
+client.user.setGame(`System Bot -help`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
